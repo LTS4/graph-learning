9460b409ce1af5fe64949134c2fc10a76a5317e3
WilliamCappelletti <cappelletti.william@gmail.com>
2023-02-08 17:54:44 +0100

    feat: prox_gdet and m-step

--- graph_learn/components.py
+++ graph_learn/components.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 from typing import Callable
 
 import numpy as np
+from numpy.random import RandomState
 from numpy.typing import NDArray
+from scipy.spatial.distance import squareform
 from sklearn.base import BaseEstimator
 
 
@@ -65,10 +67,51 @@ def primal_dual_splitting(
     return p_var, d_var
 
 
+def prox_gdet(dvar: NDArray[np.float_], sigma: float) -> NDArray[np.float_]:
+    """Proximal operator of the generalized determinant
+
+    Args:
+        dvar (NDArray[np.float_]): Stack of SPD matrices, of shape (k, n, n)
+        sigma (float): Proximal scale
+
+    Returns:
+        NDArray[np.float_]: Proximal point
+    """
+    # I have to identify the Laplacians which are unique, to speed-up computations
+    eigvals, eigvecs = np.linalg.eigh(dvar)
+    eigvals = (eigvals + np.sqrt(eigvals + 4 * sigma)) / 2
+    return np.stack(
+        [eigvec @ np.diag(eigval) @ eigvec.T for eigval, eigvec in zip(eigvals, eigvecs)]
+    )
+
+
+def laplacian_squareform(weights: NDArray[np.float]) -> NDArray[np.float_]:
+    """Get Laplacians from batch of vetorized edge weights
+
+    Args:
+        weights (NDArray[np.float]): Array of vectorized edge weights of shape (n_graphs, n_edges)
+
+    Returns:
+        NDArray[np.float_]: Laplacians stacked in array of shape (n_graphs, n_nodes, n_nodes)
+    """
+
+    return np.stack([np.diag(np.sum((lapl := squareform(w)), axis=1)) - lapl for w in weights])
+
+
+def laplacian_squareform_dual(laplacian: NDArray[np.float_]) -> NDArray[np.float_]:
+    laplacian = laplacian.copy()
+    np.fill_diagonal(laplacian, 0)
+    s = np.sum(laplacian, axis=1)
+    L1 = 2 * laplacian + s[:, np.newaxis] + s[np.newaxis, :]
+    # np.fill_diagonal(L1, 0)
+    return -squareform(L1, checks=False)
+
+
 class GraphComponents(BaseEstimator):
     def __init__(
         self,
         n_components=1,
+        alpha: float = 0.5,  # TODO: find best val
         *,
         max_iter: int = 100,
         max_iter_pds: int = 100,
@@ -79,10 +122,13 @@ class GraphComponents(BaseEstimator):
         e_tau: float = None,
         e_sigma: float = None,
         e_rho: float = None,
+        random_state: RandomState = None,
     ) -> None:
         super().__init__()
 
         self.n_components = n_components
+        self.alpha = alpha
+
         self.max_iter = max_iter
         self.max_iter_pds = max_iter_pds
         self.tol_pds = tol_pds
@@ -93,12 +139,20 @@ class GraphComponents(BaseEstimator):
         self.e_sigma = e_sigma
         self.e_rho = e_rho
 
-    def _initialize(self, x, resp):
-        pass
+        self.random_state = RandomState(random_state)
+
+        self.activations_: NDArray[np.float_]  # shape (n_components, n_samples)
+        self.weights_: NDArray[np.float_]  # shape (n_components, n_edges )
+
+    def _initialize(self, x: NDArray):
+        n_nodes, n_samples = x.shape
+
+        self.activations_ = self.random_state.rand(self.n_components, n_samples)
+        self.weights_ = self.random_state.rand(self.n_components, n_nodes * (n_nodes - 1) // 2)
 
     def _e_step(
         self,
-        activations: NDArray[np.float_],
+        # activations: NDArray[np.float_],
         # dual_var: NDArray[np.float_],
         x: NDArray[np.float_],
         laplacians: NDArray[np.float_],
@@ -106,8 +160,8 @@ class GraphComponents(BaseEstimator):
         r"""Expectation step: compute activations
 
         Args:
-            activations (NDArray[np.float_]): Initial activations
-                :math:`\Delta`, of shape (n_components, n_samples)
+            # activations (NDArray[np.float_]): Initial activations
+            #     :math:`\Delta`, of shape (n_components, n_samples)
             # dual_var (NDArray[np.float_]): Initial dual variable :math:`V =
             #     L_W \Delta`, of shape (n_samples, n_nodes, n_nodes)
             x (NDArray[np.float_]): Signals matrix, of shape (n_samples, n_nodes)
@@ -128,32 +182,53 @@ class GraphComponents(BaseEstimator):
             out = np.where(out < 1, out, 1)
             return np.where(out > 0, out, 0)
 
-        def prox_h(dvar, sigma):
-            # I have to identify the Laplacians which are unique, to speed-up computations
-            eigvals, eigvecs = np.linalg.eigh(dvar)
-            eigvals = (eigvals + np.sqrt(eigvals + 4 * sigma)) / 2
-            return np.stack(
-                [eigvec @ np.diag(eigval) @ eigvec.T for eigval, eigvec in zip(eigvals, eigvecs)]
-            )
-
-        return primal_dual_splitting(
-            activations,
-            lin_op(activations),
+        self.activations_, _ = primal_dual_splitting(
+            self.activations_,
+            lin_op(self.activations_),
             self.e_tau,
             self.e_sigma,
             self.e_rho,
             lin_op=lin_op,
             dual_op=dual_op,
             prox_g=prox_g,
-            prox_h=prox_h,
+            prox_h=prox_gdet,
             max_iter=self.max_iter_pds,
             tol=self.tol_pds,
         )
 
-    def _m_step(
-        self, w_vec: NDArray[np.float_], dual_var: NDArray[np.float_], pdists: NDArray[np.float_]
-    ):
-        pass
+    def _m_step(self, pdists: NDArray[np.float_]):
+        """Maximization step: compute weight matrices
+
+        Args:
+            pdists (NDArray[np.float_]): pairwise distances of sample-vectors,
+                shape (n_edges, n_components)
+        """
+
+        def lin_op(weights):
+            laplacians = laplacian_squareform(weights)
+            return np.matmul(laplacians.transpose(1, 2, 0), self.activations_).transpose(2, 0, 1)
+
+        def dual_op(dvar):
+            dvar = np.matmul(dvar.transpose(1, 2, 0), self.activations_.T).transpose(2, 0, 1)
+            return np.stack([laplacian_squareform_dual(laplacian) for laplacian in dvar])
+
+        def prox_g(weights, tau):
+            out = weights - tau * (pdists / 2 + self.alpha)
+            return np.where(out > 0, out, 0)
+
+        self.weights_, _ = primal_dual_splitting(
+            self.weights_,
+            lin_op(self.weights_),
+            tau=self.m_tau,
+            sigma=self.m_sigma,
+            rho=self.m_rho,
+            lin_op=lin_op,
+            dual_op=dual_op,
+            prox_g=prox_g,
+            prox_h=prox_gdet,
+            max_iter=self.max_iter_pds,
+            tol=self.tol_pds,
+        )
 
     def fit(self, x: NDArray[np.float_], _y=None) -> GraphComponents:
         pass
