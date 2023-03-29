3d834c5498915b29834b1f9228c12634d9b733be
WilliamCappelletti <cappelletti.william@gmail.com>
2023-03-29 17:33:12 +0200

    refac: simplify init

--- graph_learn/temporal_multiresolution.py
+++ graph_learn/temporal_multiresolution.py
@@ -1,44 +1,13 @@
 """Implementation of Temporal Multiresolution Graph Learning"""
 import numpy as np
-from numpy.random import RandomState
 from numpy.typing import NDArray
 
 from graph_learn.components_newer import GraphComponents
 
 
 class TMGL(GraphComponents):
-    def __init__(
-        self,
-        n_levels=1,
-        l1_weights: float = 1,
-        l1_activations: float = 1,
-        *,
-        max_iter: int = 50,
-        tol: float = 0.01,
-        max_iter_pds: int = 100,
-        tol_pds: float = 0.001,
-        pds_relaxation: float = None,
-        random_state: RandomState = None,
-        init_strategy: str = "uniform",
-        weight_prior=None,
-        discretize: bool = False,
-        verbose: int = 0,
-    ) -> None:
-        super().__init__(
-            n_components=2**n_levels - 1,
-            l1_weights=l1_weights,
-            l1_activations=l1_activations,
-            max_iter=max_iter,
-            tol=tol,
-            max_iter_pds=max_iter_pds,
-            tol_pds=tol_pds,
-            pds_relaxation=pds_relaxation,
-            random_state=random_state,
-            init_strategy=init_strategy,
-            weight_prior=weight_prior,
-            discretize=discretize,
-            verbose=verbose,
-        )
+    def __init__(self, n_levels=1, *args, **kwargs) -> None:
+        super().__init__(n_components=2**n_levels - 1, *args, **kwargs)
 
         self.n_levels = n_levels
 
