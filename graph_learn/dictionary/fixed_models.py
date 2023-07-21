23f42264befd996ab6a9c88d04cc9ccbab627a33
WilliamCappelletti <cappelletti.william@gmail.com>
2023-07-21 17:54:59 +0200

    refactor: change model formulation and include some features

--- graph_learn/dictionary/fixed_models.py
+++ graph_learn/dictionary/fixed_models.py
@@ -14,17 +14,22 @@ class FixedWeights(GraphDictionary):
     def _initialize(self, x: NDArray):
         super()._initialize(x)
 
-        if not isinstance(self.weight_prior, np.ndarray):
-            raise TypeError(
-                f"Weight prior must be a numpy array, got {type(self.activation_prior)}"
-            )
-        if self.weights_.shape != self.weight_prior.shape:
-            raise ValueError(
-                f"Invalid weight prior shape, expected {self.activations_.shape},"
-                f" got {self.weight_prior.shape}"
-            )
+        if isinstance(self.weight_prior, np.ndarray):
+            if self.weights_.shape != self.weight_prior.shape:
+                raise ValueError(
+                    f"Invalid weight prior shape, expected {self.activations_.shape},"
+                    f" got {self.weight_prior.shape}"
+                )
 
-        self.weights_ = self.weight_prior
+            self.weights_ = self.weight_prior
+
+        elif isinstance(self.activation_prior, (float, int)):
+            self.weights_ = self.weight_prior * np.ones_like(self.weights_)
+
+        else:
+            raise TypeError(
+                f"Weight prior must be a real number or a numpy array, got {type(self.activation_prior)}"
+            )
 
     def _update_weights(self, x: NDArray, mc_activations: NDArray, dual: NDArray) -> NDArray:
         return self.weights_
@@ -39,17 +44,21 @@ class FixedActivations(GraphDictionary):
     def _initialize(self, x: NDArray):
         super()._initialize(x)
 
-        if not isinstance(self.activation_prior, np.ndarray):
-            raise TypeError(
-                f"Activation prior must be a numpy array, got {type(self.activation_prior)}"
-            )
-        if self.activations_.shape != self.activation_prior.shape:
-            raise ValueError(
-                f"Invalid activation prior shape, expected {self.activations_.shape},"
-                f" got {self.activation_prior.shape}"
-            )
+        if isinstance(self.activation_prior, np.ndarray):
+            if self.activations_.shape != self.activation_prior.shape:
+                raise ValueError(
+                    f"Invalid activation prior shape, expected {self.activations_.shape},"
+                    f" got {self.activation_prior.shape}"
+                )
+            self.activations_ = self.activation_prior
 
-        self.activations_ = self.activation_prior
+        elif isinstance(self.activation_prior, (float, int)):
+            self.activations_ = self.activation_prior * np.ones_like(self.activations_)
+
+        else:
+            raise TypeError(
+                f"Activation prior must be real number or a numpy array, got {type(self.activation_prior)}"
+            )
 
     def _update_activations(self, x: NDArray, mc_activations: NDArray, dual: NDArray) -> NDArray:
         return self.activations_
