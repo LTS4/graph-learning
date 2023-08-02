bab09b3018f6f6a2068dcdb07262e1d7765fc067
WilliamCappelletti <cappelletti.william@gmail.com>
2023-08-02 17:36:46 +0200

    test: split parameters

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,7 +1,9 @@
 """Tests for bilinear operators and adjoints"""
+from itertools import product
+
 import numpy as np
+import pytest
 from numpy.random import default_rng
-from scipy.sparse.linalg import svds
 
 from graph_learn.operators import (
     laplacian_squareform,
@@ -108,57 +110,59 @@ def test_full_adjoint():
 ################################################################################
 # Operator norm tests
 
+SPACE_COMPONENTS = [1, 6, 13]
+SPACE_NODES = [20, 50, 100]
+SPACE_SAMPLES = [100, 500, 1000]
 
-def test_operator_norm_weights():
+
+@pytest.mark.parametrize(
+    "n_components,n_nodes,n_samples", list(product(SPACE_COMPONENTS, SPACE_NODES, SPACE_SAMPLES))
+)
+def test_operator_norm_weights(n_components, n_nodes, n_samples):
     """Verify that the operator norm, wrt weights is correct"""
 
-    for seed in range(10):
-        rng = default_rng(seed)
-        n_components = rng.integers(1, 10)
-        n_nodes = rng.integers(20, 100)
-        n_samples = rng.integers(100, 1000)
+    rng = default_rng(n_components * n_nodes * n_samples)
 
-        activations = rng.standard_normal((n_components, n_samples))
-        weights = rng.standard_normal((n_components, (n_nodes * (n_nodes - 1)) // 2))
+    activations = rng.standard_normal((n_components, n_samples))
+    weights = rng.standard_normal((n_components, (n_nodes * (n_nodes - 1)) // 2))
 
-        estimate = op_weights_norm(activations=activations, n_nodes=n_nodes) ** 2
+    estimate = op_weights_norm(activations=activations, n_nodes=n_nodes) ** 2
 
-        for _ in range(100):
-            weights = weights / np.linalg.norm(weights)
-            _weights = op_adj_weights(
-                activations,
-                np.einsum("kt,knm->tnm", activations, laplacian_squareform_vec(weights)),
-            )
+    for _ in range(n_samples):
+        weights = weights / np.linalg.norm(weights)
+        _weights = op_adj_weights(
+            activations,
+            np.einsum("kt,knm->tnm", activations, laplacian_squareform_vec(weights)),
+        )
 
-            alpha = np.mean(_weights / weights)
-            weights = _weights
+        alpha = np.mean(_weights / weights)
+        weights = _weights
 
-        assert np.isclose(estimate, alpha, rtol=1e-2)
+    assert np.isclose(estimate, alpha, rtol=1e-2)
 
 
-def test_operator_norm_activations():
+@pytest.mark.parametrize(
+    "n_components, n_nodes, n_samples", list(product(SPACE_COMPONENTS, SPACE_NODES, SPACE_SAMPLES))
+)
+def test_operator_norm_activations(n_components, n_nodes, n_samples):
     """Verify that the operator norm, wrt activations is correct"""
 
-    for seed in range(10):
-        rng = default_rng(seed)
-        n_components = rng.integers(1, 10)
-        n_nodes = rng.integers(20, 100)
-        n_samples = rng.integers(100, 1000)
+    rng = default_rng(n_components * n_nodes * n_samples)
 
-        activations = rng.standard_normal((n_components, n_samples))
-        weights = rng.standard_normal((n_components, (n_nodes * (n_nodes - 1)) // 2))
+    activations = rng.standard_normal((n_components, n_samples))
+    weights = rng.standard_normal((n_components, (n_nodes * (n_nodes - 1)) // 2))
 
-        lapl = laplacian_squareform_vec(weights)
-        estimate = op_activations_norm(lapl) ** 2
+    lapl = laplacian_squareform_vec(weights)
+    estimate = op_activations_norm(lapl) ** 2
 
-        for _ in range(200):
-            activations = activations / np.linalg.norm(activations)
-            _activations = op_adj_activations(
-                weights,
-                np.einsum("kt,knm->tnm", activations, laplacian_squareform_vec(weights)),
-            )
+    for _ in range(200):
+        activations = activations / np.linalg.norm(activations)
+        _activations = op_adj_activations(
+            weights,
+            np.einsum("kt,knm->tnm", activations, laplacian_squareform_vec(weights)),
+        )
 
-            alpha = np.mean(_activations / activations)
-            activations = _activations
+        alpha = np.mean(_activations / activations)
+        activations = _activations
 
-        assert np.isclose(estimate, alpha, rtol=1e-2)
+    assert np.isclose(estimate, alpha, rtol=1e-2)
