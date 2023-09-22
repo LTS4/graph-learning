e6653146051e41064080689f50e25e2ea1fe0940
WilliamCappelletti <cappelletti.william@gmail.com>
2023-09-22 14:51:31 +0200

    refac!: remove old components code

--- graph_learn/components/__init__.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-module-docstring
-__all__ = [
-    "GraphComponents",
-    "FixedActivations",
-    "FixedWeights",
-]
-
-from .base_model import GraphComponents
-from .fixed_models import FixedActivations, FixedWeights
