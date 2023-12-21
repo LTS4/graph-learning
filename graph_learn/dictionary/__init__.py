d6afef9cf1695ff04205cb17749e36fcdb3c7f3d
WilliamCappelletti <cappelletti.william@gmail.com>
2023-12-21 10:43:44 +0100

    refact!: rename model to avod disambiguation

--- graph_learn/dictionary/__init__.py
+++ graph_learn/dictionary/__init__.py
@@ -1,9 +1,11 @@
 # pylint: disable=missing-module-docstring
 __all__ = [
     "GraphDictionary",
+    "GraphDictExact",
     "FixedActivations",
     "FixedWeights",
 ]
 
 from .base_model import GraphDictionary
+from .exact_model import GraphDictExact
 from .fixed_models import FixedActivations, FixedWeights
