72ccf9e5d49b9f1eaaca218bb06e9effdb3511f7
WilliamCappelletti <cappelletti.william@gmail.com>
2024-01-29 19:13:02 +0100

    feat: implement model with degree logbarrier

--- graph_learn/dictionary/__init__.py
+++ graph_learn/dictionary/__init__.py
@@ -2,6 +2,7 @@
 __all__ = [
     "GraphDictionary",
     "GraphDictExact",
+    "GraphDictLog",
     "FixedActivations",
     "FixedWeights",
 ]
@@ -9,3 +10,4 @@ __all__ = [
 from .combi_model import GraphDictionary
 from .exact_model import GraphDictExact
 from .fixed_models import FixedActivations, FixedWeights
+from .log_model import GraphDictLog
