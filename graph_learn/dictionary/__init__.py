ceddfd01fb67ffd902098a392fbc080a64bbab17
WilliamCappelletti <cappelletti.william@gmail.com>
2024-05-29 18:20:07 +0200

    feat: spectral dictionary model

--- graph_learn/dictionary/__init__.py
+++ graph_learn/dictionary/__init__.py
@@ -1,14 +1,16 @@
 # pylint: disable=missing-module-docstring
 __all__ = [
-    "GraphDictionary",
+    "GraphDictBase",
     "GraphDictExact",
-    "GraphDictLog",
     "GraphDictHier",
+    "GraphDictLog",
+    "GraphDictSpectral",
     "FixedActivations",
     "FixedWeights",
 ]
 
-from .combi_model import GraphDictionary
+from .base_model import GraphDictBase
 from .exact_model import GraphDictExact
 from .fixed_models import FixedActivations, FixedWeights, GraphDictHier
 from .log_model import GraphDictLog
+from .spectral_model import GraphDictSpectral
