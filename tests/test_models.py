8f02964fbf18b3f39f1ed43e23a37f5f0dc897e6
WilliamCappelletti <cappelletti.william@gmail.com>
2024-09-20 12:02:24 +0200

    test: test windowlog

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,7 @@ import pytest
 from numpy.random import default_rng
 
 from graph_learn.dictionary import GraphDictLog
+from graph_learn.temporal.smooth import WindowLogModel
 
 
 @pytest.fixture
@@ -20,3 +21,8 @@ def data(seed):
 @pytest.mark.parametrize("n_atoms, l1_w", list(product([1, 3], [0.0, 0.01])))
 def test_graphdictlog_fit_l1_w(n_atoms, l1_w, data, seed):
     GraphDictLog(n_atoms=n_atoms, l1_w=l1_w, random_state=seed, max_iter=100).fit(data)
+
+
+@pytest.mark.parametrize("window_size, avg_degree", list(product([5, 10], [2, 5])))
+def test_windowlogmodel_fit(window_size, avg_degree, data, seed):
+    WindowLogModel(window_size=window_size, avg_degree=avg_degree, random_state=seed).fit(data)
