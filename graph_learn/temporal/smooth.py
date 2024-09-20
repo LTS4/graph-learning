ce78b3a2adbdae6d9a6b941ffe5aed2a4546b5d2
WilliamCappelletti <cappelletti.william@gmail.com>
2024-09-20 12:05:08 +0200

    refact: windowlogmodel inherits from logmodel

--- graph_learn/temporal/smooth.py
+++ graph_learn/temporal/smooth.py
@@ -7,17 +7,17 @@ from numpy.typing import NDArray
 from sklearn.base import BaseEstimator
 
 from graph_learn.operators import square_to_vec
-from graph_learn.smooth_learning import get_theta, gsp_learn_graph_log_degrees
+from graph_learn.smooth_learning import LogModel, get_theta, gsp_learn_graph_log_degrees
 
 
-class WindowLogModel(BaseEstimator):
+class WindowLogModel(LogModel):
     """Learn a sequence of graphs from signals by applying LogModel on windows"""
 
     def __init__(
         self,
         window_size: int = 10,
+        avg_degree: int = None,
         *,
-        avg_degree: float = None,
         edge_init: Optional[NDArray[np.float64]] = None,
         maxit: int = 1000,
         tol: float = 1e-5,
@@ -25,17 +25,20 @@ class WindowLogModel(BaseEstimator):
         edge_tol: float = 1e-3,
         random_state=None,
     ) -> None:
+        super().__init__(
+            avg_degree=avg_degree,
+            edge_init=edge_init,
+            maxit=maxit,
+            tol=tol,
+            step_size=step_size,
+            edge_tol=edge_tol,
+        )
         self.window_size = window_size
-        self.avg_degree = avg_degree
-        self.edge_init = edge_init
-        self.maxit = maxit
-        self.tol = tol
-        self.step_size = step_size
-        self.edge_tol = edge_tol
 
+        self.theta_: NDArray[np.float64]
         self.weights_: NDArray[np.float64]
 
-    def fit(self, x: NDArray[np.float64]):
+    def _initialize(self, x) -> NDArray[np.float64]:
         n_samples, n_nodes = x.shape
 
         if (pad_size := n_samples % self.window_size) > 0:
@@ -50,8 +53,18 @@ class WindowLogModel(BaseEstimator):
             axis=1,
         )
 
+        if self.avg_degree is None:
+            self.theta_ = 1
+        else:
+            self.theta_ = np.array([[get_theta(sqpd, self.avg_degree)] for sqpd in sq_pdists])
+
+        return square_to_vec(sq_pdists)
+
+    def fit(self, x: NDArray[np.float64]):
+        sq_pdists = self._initialize(x)
+
         self.weights_ = gsp_learn_graph_log_degrees(
-            square_to_vec(sq_pdists) * [[get_theta(sqpd, self.avg_degree)] for sqpd in sq_pdists],
+            sq_pdists * self.theta_,
             1,
             1,
             edge_init=self.edge_init,
