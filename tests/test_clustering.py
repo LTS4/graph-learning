31bb0a220ad3ac850292b4d2998461f8a5d056a7
WilliamCappelletti <cappelletti.william@gmail.com>
2024-10-24 15:22:13 +0200

    test: compare metrics to baseline

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -6,17 +6,18 @@ import numpy as np
 import pytest
 from numpy.random import default_rng
 from sklearn.metrics import adjusted_mutual_info_score
+from sklearn.mixture import GaussianMixture
 
 from graph_learn.clustering import GLMM, KGraphs, KGraphsV2
 from graph_learn.sampling.graphs import sample_er_laplacian
 from graph_learn.sampling.signals import sample_lgmrf
 
 N_NODES = 13
-N_SAMPLES = 100
+N_SAMPLES = 1000
 N_CLUSTERS = 2
 EDGE_P = 0.2
 
-MIN_SCORE = 0
+MAX_ITER = 1000
 
 
 @pytest.fixture
@@ -40,49 +41,80 @@ def data(y_true, rng):
 
     for i, lapl in enumerate(laplacians):
         mask = y_true == i
-        x[mask] = sample_lgmrf(lapl, n_samples=mask.sum(), seed=rng)
+        x[mask] = 2 * rng.standard_normal(size=(mask.sum(), N_NODES)) + sample_lgmrf(
+            lapl, n_samples=mask.sum(), seed=rng
+        )
 
     return x
 
 
-def test_glmm(data, y_true, rng):
+@pytest.fixture
+def min_score(data, y_true, rng):
+    """Compute baseline (Gaussian Mixture) score."""
+    model = GaussianMixture(
+        n_components=N_CLUSTERS,
+        n_init=5,
+        max_iter=MAX_ITER,
+        random_state=rng.integers(10**6),
+    )
+
+    y_pred = model.fit_predict(data)
+
+    # This is ~6.9e-4
+    return adjusted_mutual_info_score(y_true, y_pred)
+
+
+def test_glmm(data, y_true, min_score, rng):
     """Test GLMM clustering"""
     model = GLMM(
         n_components=N_CLUSTERS,
         avg_degree=int(EDGE_P * N_NODES) + 1,
         n_init=5,
+        max_iter=MAX_ITER,
         random_state=rng.integers(10**6),
     )
 
     y_pred = model.fit_predict(data)
 
-    # assert np.mean(y_true == y_pred) > MIN_SCORE
-    assert adjusted_mutual_info_score(y_true, y_pred) > MIN_SCORE
+    # This is ~3.7e-3
+    assert adjusted_mutual_info_score(y_true, y_pred) > min_score
 
 
-def test_kgraphs(data, y_true, rng):
+def test_kgraphs(data, y_true, min_score, rng):
     """Test KGraphs clustering"""
     model = KGraphs(
         n_clusters=N_CLUSTERS,
         avg_degree=int(EDGE_P * N_NODES) + 1,
         n_init=5,
+        max_iter=MAX_ITER,
         random_state=rng.integers(10**6),
     )
 
     y_pred = model.fit_predict(data)
 
-    # assert np.mean(y_true == y_pred) > MIN_SCORE
-    assert adjusted_mutual_info_score(y_true, y_pred) > MIN_SCORE
+    # This is ~5.1e-3
+    assert adjusted_mutual_info_score(y_true, y_pred) > min_score
 
 
-def test_kgraphsv2(data, y_true, rng):
+def test_kgraphsv2(data, y_true, min_score, rng):
     """Test KGraphsV2 clustering"""
     model = KGraphsV2(
         n_clusters=N_CLUSTERS,
         avg_degree=int(EDGE_P * N_NODES) + 1,
+        n_init=5,
+        max_iter=MAX_ITER,
         random_state=rng.integers(10**6),
     )
 
     y_pred = model.fit_predict(data)
 
-    assert adjusted_mutual_info_score(y_true, y_pred) > MIN_SCORE
+    # This is ~1.2e-2
+    assert adjusted_mutual_info_score(y_true, y_pred) > min_score
+
+
+# array([[-0.14153019, -0.22397099,  0.22772982,  0.06249811, -0.06384377,
+#          0.05714588,  0.51780199,  0.39499243,  0.02803168, -0.18984876,
+#         -0.48554256, -0.10164939, -0.08181426],
+#        [ 0.17566147,  0.05249482, -0.28870163,  0.14559845, -0.01851911,
+#          0.15573786, -0.87927826, -0.23088974,  0.09902922,  0.10506065,
+#          0.26892766,  0.10224784,  0.31263077]])
